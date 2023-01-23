2e27cd48742e2e7eb553168b967a6969957da199
chrissMcKenzie <chrissMcKenzie.IT.Agence@gmail.com>
2023-01-23 17:03:45 +0100

    Conflit: Categorie.jsx

--- front/src/containers/Categorie.jsx
+++ front/src/containers/Categorie.jsx
@@ -6,17 +6,16 @@ export default function Categorie({data}) {
  
   return (
       <div className="row row-cols-1 row-cols-md-3 g-4">
-      
-   <div className="col">
-      <div className="card h-100">
-        <img src={jeuVideo} className="card-img-top" alt="..."></img>
-        <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <p>Prix:15$</p>
-          <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        </div>
+        <div className="col">
+          <div className="card h-100">
+            <img src={JeuVideo} className="card-img-top" alt="..."></img>
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p>Prix:15$</p>
+              <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+          </div>
+        </div> 
       </div>
-    </div> 
-  </div>
   )
 }
