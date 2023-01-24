928f700d0986c656c836826a5f84efc383aa93a6
chrissMcKenzie <chrissMcKenzie.IT.Agence@gmail.com>
2023-01-24 23:46:45 +0100

    Conflit: Accueil.jsx => je rechange les import 3

--- front/src/containers/Accueil.jsx
+++ front/src/containers/Accueil.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { Link } from "react-router-dom";
 import {Container, Row, Card, Col} from 'react-bootstrap';
-import { useState, useEffect } from 'react'
 
 //* Pages
 import "./Accueil.css"
