30d52199916d87cacc35611e07fd8fa0fcfd3669
chrissMcKenzie <chrissMcKenzie.IT.Agence@gmail.com>
2023-01-27 17:58:02 +0100

    Update: Payement.jsx

--- front/src/components/React-Bootstrap/FormulaireDePaiement.jsx
+++ front/src/components/React-Bootstrap/FormulaireDePaiement.jsx
@@ -17,57 +17,56 @@ function FormulaireDePaiement() {
                             </h2>
                             <div className="mb-3">
                                 <Form method="POST" action="/Inscription">
-                                <Row>
-                                    <Form.Group as={Col} className="mb-4" controlId="Username" >
-                                        <Form.Label className="text-center"> Nom du Titulaire </Form.Label>
-                                        <Form.Control type="text" name="Username" placeholder="Nom du Titulaire ?"
-                                            // onChange={(e) => setPrenom(e.target.value)}
-                                        />
-                                    </Form.Group>
-                                </Row>
+                                    <Row>
+                                        <Form.Group as={Col} className="mb-4" controlId="Username" >
+                                            <Form.Label className="text-center"> Nom du Titulaire </Form.Label>
+                                            <Form.Control type="text" name="Username" placeholder="Nom du Titulaire ?"
+                                                // onChange={(e) => setPrenom(e.target.value)}
+                                            />
+                                        </Form.Group>
+                                    </Row>
 
-                                <Row>
-                                    <Form.Group className="mb-4" controlId="Adresse">
-                                        <Form.Label className="text-center">Numéro de carte</Form.Label>
-                                        <Form.Control type="number" name="Adresse" placeholder="Numéro de carte ?"
-                                        // onChange={updateInput}
-                                        />
-                                    </Form.Group>
-                                </Row>
-
-                                <Row>
-                                    <Form.Group as={Col} className="mb-4" controlId="DateNaissance">
-                                        <Form.Label className="text-center"> Expiry date </Form.Label>
-                                        <Form.Control type="Date" name="DateDeNaissance" placeholder="Date de Naissance ?"
+                                    <Row>
+                                        <Form.Group className="mb-4" controlId="Adresse">
+                                            <Form.Label className="text-center">Numéro de carte</Form.Label>
+                                            <Form.Control type="number" name="Adresse" placeholder="Numéro de carte ?"
                                             // onChange={updateInput}
-                                        />
-                                    </Form.Group>
+                                            />
+                                        </Form.Group>
+                                    </Row>
 
-                                    <Form.Group as={Col} className="mb-4" controlId="DateNaissance">
-                                        <Form.Label className="text-center"> Security Code </Form.Label>
-                                        <Form.Control type="number" name="DateDeNaissance" placeholder="Security Code ?" className="is-validate"
+                                    <Row>
+                                        <Form.Group as={Col} className="mb-4" controlId="DateNaissance">
+                                            <Form.Label className="text-center"> Expiry date </Form.Label>
+                                            <Form.Control type="Date" name="DateDeNaissance" placeholder="Date de Naissance ?"
+                                                // onChange={updateInput}
+                                            />
+                                        </Form.Group>
+
+                                        <Form.Group as={Col} className="mb-4" controlId="DateNaissance">
+                                            <Form.Label className="text-center"> Security Code </Form.Label>
+                                            <Form.Control type="number" name="DateDeNaissance" placeholder="Security Code ?" className="is-validate"
+                                                // onChange={updateInput}
+                                            />
+                                        </Form.Group>
+                                    </Row>
+
+                                    <Row>
+                                        <Form.Group as={Col} className="mb-4" controlId="CODEPOSTAL">
+                                            <Form.Label className="text-center"> Code Postal </Form.Label>
+                                            <Form.Control type="number" name="CodePostal" placeholder="Code Postal?"
                                             // onChange={updateInput}
-                                        />
-                                    </Form.Group>
-                                </Row>
-
-                                <Row>
-                                    <Form.Group as={Col} className="mb-4" controlId="CODEPOSTAL">
-                                        <Form.Label className="text-center"> Code Postal </Form.Label>
-                                        <Form.Control type="number" name="CodePostal" placeholder="Code Postal?"
-                                        // onChange={updateInput}
-                                        />
-                                    </Form.Group>
-                                </Row>
-                        
-                                <Form.Group className="mb-3" controlId="formBasicCheckbox"></Form.Group>
-                                <div className="d-grid">
-                                    <Button variant="warning" size="lg" type="submit" style ={{backgroundColor:'#d3b94f', border:'0'}}
-                                    // onChange={handleSubmit}
-                                    >
-                                    PAIEMENT 100€
-                                    </Button>
-                                </div>
+                                            />
+                                        </Form.Group>
+                                    </Row>
+                            
+                                    <div className="d-grid">
+                                        <Button variant="warning" size="lg" type="submit" style ={{backgroundColor:'#d3b94f', border:'0'}}
+                                        // onChange={handleSubmit}
+                                        >
+                                        PAIEMENT 100€
+                                        </Button>
+                                    </div>
                                 </Form>
                             </div>
                         </div>
